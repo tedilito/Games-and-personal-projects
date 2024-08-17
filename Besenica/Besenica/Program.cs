7b539fc377a4f3ac23210c5241ddecf619cb0a33
Teodor Ivanov <156572502+tedilito@users.noreply.github.com>
2024-08-17 13:40:40 +0300

    Add files via upload

---
+++ Besenica/Besenica/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Hangman
+{
+    static void Main()
+    {
+        string[] words = { "PTG", "chovek", "lujica", "c#", "Gabrovo" };
+        Random random = new Random();
+        string wordToGuess = words[random.Next(0, words.Length)];
+        char[] guessedWord = new string('_', wordToGuess.Length).ToCharArray();
+        List<char> incorrectGuesses = new List<char>();
+        int attempts = 6;
+
+        Console.WriteLine("Welcome to Hangman!");
+
+        while (attempts > 0 && new string(guessedWord) != wordToGuess)
+        {
+            Console.WriteLine($"Word: {new string(guessedWord)}");
+            Console.WriteLine($"Incorrect guesses: {string.Join(", ", incorrectGuesses)}");
+            Console.WriteLine($"Attempts left: {attempts}");
+            Console.Write("Guess a letter: ");
+            char guess = Console.ReadLine()[0];
+
+            if (wordToGuess.Contains(guess))
+            {
+                for (int i = 0; i < wordToGuess.Length; i++)
+                {
+                    if (wordToGuess[i] == guess)
+                    {
+                        guessedWord[i] = guess;
+                    }
+                }
+            }
+            else
+            {
+                incorrectGuesses.Add(guess);
+                attempts--;
+            }
+        }
+
+        if (new string(guessedWord) == wordToGuess)
+        {
+            Console.WriteLine($"Congratulations! You guessed the word: {wordToGuess}");
+        }
+        else
+        {
+            Console.WriteLine($"Game over! The word was: {wordToGuess}");
+        }
+    }
+}
---
+++ NokiaSnake/NokiaSnake/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+class SnakeGame
+{
+    static int width = 20;
+    static int height = 20;
+    static int score = 0;
+    static List<(int, int)> snake = new List<(int, int)>();
+    static (int, int) food;
+    static (int, int) direction = (0, 1);
+    static Random random = new Random();
+
+    static void Main()
+    {
+        Console.CursorVisible = false;
+        snake.Add((width / 2, height / 2));
+        GenerateFood();
+
+        while (true)
+        {
+            if (Console.KeyAvailable)
+            {
+                var key = Console.ReadKey(true).Key;
+                direction = key switch
+                {
+                    ConsoleKey.UpArrow => (-1, 0),
+                    ConsoleKey.DownArrow => (1, 0),
+                    ConsoleKey.LeftArrow => (0, -1),
+                    ConsoleKey.RightArrow => (0, 1),
+                    _ => direction
+                };
+            }
+
+            MoveSnake();
+            if (CheckCollision())
+            {
+                break;
+            }
+
+            Draw();
+            Thread.Sleep(100);
+        }
+
+        Console.Clear();
+        Console.WriteLine($"Game Over! Your score: {score}");
+    }
+
+    static void MoveSnake()
+    {
+        var head = snake[0];
+        var newHead = (head.Item1 + direction.Item1, head.Item2 + direction.Item2);
+        snake.Insert(0, newHead);
+
+        if (newHead == food)
+        {
+            score++;
+            GenerateFood();
+        }
+        else
+        {
+            snake.RemoveAt(snake.Count - 1);
+        }
+    }
+
+    static bool CheckCollision()
+    {
+        var head = snake[0];
+        if (head.Item1 < 0 || head.Item1 >= height || head.Item2 < 0 || head.Item2 >= width)
+        {
+            return true;
+        }
+
+        for (int i = 1; i < snake.Count; i++)
+        {
+            if (snake[i] == head)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    static void GenerateFood()
+    {
+        food = (random.Next(0, height), random.Next(0, width));
+    }
+
+    static void Draw()
+    {
+        Console.Clear();
+        for (int i = 0; i < height; i++)
+        {
+            for (int j = 0; j < width; j++)
+            {
+                if (snake.Contains((i, j)))
+                {
+                    Console.Write("O");
+                }
+                else if (food == (i, j))
+                {
+                    Console.Write("X");
+                }
+                else
+                {
+                    Console.Write(".");
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
